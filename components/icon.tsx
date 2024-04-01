5276481af2ee78edbb2e62fb24ff606435fa0ddc
banuka <banukaramanayaka76@gmail.com>
2024-04-01 22:06:59 +0530

    feat:svg optimization with sly-cli

---
+++ components/icon.tsx
@@ -0,0 +1,40 @@
+import { type SVGProps } from 'react'
+
+import { type IconName } from '@/types/name'
+
+import { cn } from '@/lib/utils'
+
+export { IconName }
+
+export function Icon({
+    name,
+    childClassName,
+    className,
+    children,
+    ...props
+}: SVGProps<SVGSVGElement> & {
+    name: IconName
+    childClassName?: string
+}) {
+    if (children) {
+        return (
+            <span
+                className={cn(
+                    `inline-flex items-center font gap-1.5`,
+                    childClassName
+                )}
+            >
+                <Icon name={name} className={className} {...props} />
+                {children}
+            </span>
+        )
+    }
+    return (
+        <svg
+            {...props}
+            className={cn('inline self-center w-[1em] h-[1em]', className)}
+        >
+            <use href={`./icons/sprite.svg#${name}`} />
+        </svg>
+    )
+}
