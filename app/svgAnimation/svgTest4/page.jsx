870272667bbeec06df632941380dd5f5d321a5fc
banuka <banukaramanayaka76@gmail.com>
2024-04-04 22:57:43 +0530

    other:try 2 no use

--- app/svgAnimation/svgTest4/page.jsx
+++ app/svgAnimation/svgTest4/page.jsx
@@ -1,21 +1,27 @@
 'use client'
 
 import { IconJsx } from '@/components/iconJsx'
-import { useEffect } from 'react'
+import { useAnimate } from 'framer-motion'
+import { useEffect, useRef } from 'react'
 
 export default function page() {
+    const svgSprite = useRef()
+    const [_, animate] = useAnimate()
     useEffect(() => {
-        fetch('http://localhost:3000/icons/sprite.svg')
-            .then((res) => res.body)
-            .then((data) => console.log(data))
-
-        // console.log(svgSprite)
+        console.log(svgSprite)
+        animate(
+            svgSprite.current,
+            { scale: 120 },
+            { ease: 'easeInOut', duration: 10 }
+        )
         return () => {}
     }, [])
 
     return (
         <div>
-            <IconJsx className="h-52 w-52" name="Frame 1" />
+            <svg>
+                <IconJsx ref={svgSprite} className="h-52 w-52" name="Frame 1" />
+            </svg>
         </div>
     )
 }
