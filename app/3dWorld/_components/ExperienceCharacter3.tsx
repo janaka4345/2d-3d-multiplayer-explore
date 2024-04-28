319dcd584bb6ec8ae884811818911a225b81a69c
banuka <banukaramanayaka76@gmail.com>
2024-04-28 23:40:16 +0530

    feat:rapier kinematic body movements  test

--- app/3dWorld/_components/ExperienceCharacter3.tsx
+++ app/3dWorld/_components/ExperienceCharacter3.tsx
@@ -1,4 +1,4 @@
-import { CapsuleCollider, Physics, type RapierRigidBody, RigidBody } from "@react-three/rapier"
+import { CapsuleCollider, Physics, type RapierRigidBody, RigidBody, CuboidCollider } from "@react-three/rapier"
 import Character from "./Character"
 import Plane from "./Plane"
 import Cube from "./Cube"
@@ -13,87 +13,54 @@ const ExperienceCharacter = () => {
 
 
     const characterRef = useRef<RapierRigidBody>()
+    console.log(' ran');
 
     useEffect(() => {
+        console.log('useeffect ran');
 
-
-        // const unsubJumpSub = sub(
-        //     (state) => state.jump,
-        //     (pressed) => {
-        //         console.log('jump', pressed)
-        //     }
-        // )
-        // const unsubForwardSub = sub(
-        //     (state) => state.forward,
-        //     (pressed) => {
-        //         console.log('forward', pressed)
-        //     }
-        // )
-        // const unsubBackSub = sub(
-        //     (state) => state.back,
-        //     (pressed) => {
-        //         console.log('back', pressed)
-        //     }
-        // )
-        // const unsubLeftSub = sub(
-        //     (state) => state.left,
-        //     (pressed) => {
-        //         console.log('left', pressed)
-        //     }
-        // )
-        // const unsubRightSub = sub(
-        //     (state) => state.right,
-        //     (pressed) => {
-        //         console.log('right', pressed)
-        //     }
-        // )
-
-
-        // return () => {
-        //     unsubJumpSub()
-        //     unsubForwardSub()
-        //     unsubBackSub()
-        //     unsubLeftSub()
-        //     unsubRightSub()
-        //     console.log('close ran');
-
-        // }
     }, [])
-
     useFrame((state) => {
         const jump = get().jump
+        const forward = get().forward
+        const back = get().backward
+        const left = get().left
+        const right = get().ArrowRight
+        const impulse = { x: 0.5, y: 0, z: 0 }
+        const torque = { x: 0, y: 0.5, z: 0 }
 
-        // console.log({ jumpPressed, forwardPressed, backPressed, leftPressed, rightPressed });
-        console.log(jump);
 
+        if (left) {
+            // characterRef.current.applyTorqueImpulse(torque, true)
+            // characterRef.current.setRotation({ x: 5, y: 8, z: 6, w: 7 }, true)
+            // characterRef.current.setLinvel({ x: 1, y: 0, z: 0 }, true)
+            // characterRef.current.setAngvel({ x: 3.0, y: 0.0, z: 0.0 }, true)
+            characterRef.current.setNextKinematicTranslation({ x: characterRef.current.translation().x + 0.01, y: 0.5, z: 0 })
+            // characterRef.current.setNextKinematicPosition({ x: 1, y: 0, z: 0 })
+
+        }
+        if (forward) {
+            // characterRef.current.applyImpulse(impulse, true)
+
+        }
+        // characterRef.current.applyImpulse(impulse, true)
+        // characterRef.current.applyTorqueImpulse(torque, true)
 
     })
-    useEffect(() => {
 
-        // return sub(
-        //     (state) => state.forward,
-        //     (pressed) => {
-        //         console.log('forward', pressed)
-        //         // characterRef.current.add
-
-        //     }
-        // )
-
-
-    }, [])
     return (
         <>
             <Physics debug >
-                <RigidBody friction={1} restitution={0}>
+                <RigidBody type="fixed" friction={1} restitution={0}>
 
                     <Plane />
                 </RigidBody>
-                {/* <RigidBody friction={0.4} restitution={0.5} position={[0, 1, 0]}>
+                <RigidBody friction={0.4} restitution={0.5} position={[0, 1, 0]}>
                     <Cube />
-                </RigidBody> */}
-                <RigidBody ref={characterRef} type="kinematicPosition" friction={0.7} restitution={1} position={[0, 0.5, 0]} colliders={false}>
+                </RigidBody>
+                <RigidBody type="kinematicPosition" ref={characterRef} friction={1} restitution={0} position={[0, 0.5, 0]} colliders={false}>
                     <Character castShadow position={[0, -1.5, 0]} />
                     <CapsuleCollider args={[1.05, 0.5]} />
+                    {/* <CuboidCollider args={[0.5, 0.5, 0.5]} /> */}
                 </RigidBody>
 
             </Physics>
