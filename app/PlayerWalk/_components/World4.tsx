15ca3f389fbe3c35a9a474f1289f712bef4b1351
banuka <banukaramanayaka76@gmail.com>
2024-04-30 20:03:54 +0530

    feat:another  gltf player added for testing

--- app/PlayerWalk/_components/World4.tsx
+++ app/PlayerWalk/_components/World4.tsx
@@ -2,7 +2,8 @@ import { OrbitControls } from "@react-three/drei";
 // import Lights from "./Lights";
 import { Physics, RigidBody } from "@react-three/rapier";
 import Plane from "@/app/3dWorld/_components/Plane";
-import Player from "./PlayerReal";
+// import Player from "./PlayerReal";
+import Player from "./PlayerRealOptm";
 import { useKeyboard } from "./useKeyboard";
 import { useMouseCapture } from "./useMouseCapture";
 import { Perf } from "r3f-perf";
@@ -16,10 +17,15 @@ function getInput(keyboard, mouse) {
     if (keyboard["a"]) x -= 0.2;
     if (keyboard[" "]) y += 0.2;
 
+
+    // console.log({ move: [x, y, z], look: [mouse.x / window.innerWidth, mouse.y / window.innerHeight] });
+    // console.log('default', { x, y, z });
+    // console.log({ x: mouse.x, y: mouse.y });
     return {
         move: [x, y, z],
         look: [mouse.x / window.innerWidth, mouse.y / window.innerHeight],
         running: keyboard["Shift"],
+
     };
 }
 export default function World() {
@@ -28,7 +34,8 @@ export default function World() {
 
     return (
         <>
-            <OrbitControls makeDefault />
+
+            {/* <OrbitControls makeDefault /> */}
             <Perf position="top-right" />
             {/* <OrbitControls /> */}
             <directionalLight intensity={2} castShadow position={[5, 5, 5]} />
@@ -40,6 +47,7 @@ export default function World() {
                 <RigidBody type="fixed" >
                     <Plane /></RigidBody>
                 {/* <Terrain /> */}
+                {/* <Player walk={0.2} jump={0.2} input={() => getInput(keyboard, mouse)} /> */}
                 <Player walk={0.2} jump={0.2} input={() => getInput(keyboard, mouse)} />
             </Physics>
         </>
